45903b57823f2fc3a283506b1788402c0461848a
Joshua Kennedy <josh@outthinkgroup.com>
2021-05-28 14:23:14 -0400

    try3

--- functions/shareImage-dev.js
+++ functions/shareImage-dev.js
@@ -1,5 +1,5 @@
 const puppeteer = require("puppeteer");
-const { webScrape } = require("./src/webscrape.js");
+const { webScrape } = require("../src/webscrape.js");
 
 exports.handler = (event, context) =>
   webScrape({ event, context }, puppeteer, false);
--- functions/shareImage.js
+++ functions/shareImage.js
@@ -1,6 +1,6 @@
 const puppeteer = require("puppeteer-core");
 const { builder } = require("@netlify/functions");
-const { webScrape } = require("./src/webscrape.js");
+const { webScrape } = require("../src/webscrape.js");
 const handler = async (event, context) => {
   res = await webScrape({ event, context }, puppeteer, true);
   return res;
